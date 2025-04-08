bcb3d5a9b9859eb5ff30ae69cecb24b669eade0c
Veda <peddireddyvedasamhitha@gmail.com>
2025-04-09 01:56:56 +0530

    hi

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin:"https://library-management-system-plum-delta.vercel.app",
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true,
   })
