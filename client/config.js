493160a3411fc0a8ccdfb9e467b4604fbba44968
Veda <peddireddyvedasamhitha@gmail.com>
2025-04-09 01:48:35 +0530

    hello

--- client/config.js
+++ client/config.js
@@ -1,1 +1,2 @@
-export const Frontend_URL = "http://localhost:4000/api/v1";
+// export const Frontend_URL = "http://localhost:4000/api/v1";
+export const Frontend_URL = "https://library-management-system-mvsy.vercel.app/api/v1";
