22e5ce643c8ad3ba970cba3be8b895a62edd575d
Veda <peddireddyvedasamhitha@gmail.com>
2025-05-07 20:11:14 +0530

    Connection of render and vercel

--- client/config.js
+++ client/config.js
@@ -1,2 +1,3 @@
 // export const Frontend_URL = "http://localhost:4000/api/v1";
-export const Frontend_URL = "https://library-management-system-mvsy.vercel.app/api/v1";
+//export const Frontend_URL = "https://library-management-system-mvsy.vercel.app/api/v1";
+export const API_URL = import.meta.env.VITE_API_URL;
