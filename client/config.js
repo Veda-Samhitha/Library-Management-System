c2b408eff4aee818865a3a3a9918829ebac0a61d
Veda <peddireddyvedasamhitha@gmail.com>
2025-05-07 20:56:51 +0530

    Replaced

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
 // export const Frontend_URL = "http://localhost:4000/api/v1";
-//export const Frontend_URL = "https://library-management-system-mvsy.vercel.app/api/v1";
-export const Frontend_URL = "https://library-management-system-plum-delta.vercel.app";
+//export const Frontend_URL = "https://library-management-system-plum-delta.vercel.app";
+export const Frontend_URL = "https://library-management-system-31ue.onrender.com";
 export const API_URL = import.meta.env.VITE_API_URL;
